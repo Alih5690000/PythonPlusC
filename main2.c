803942bd6f7e11379ebbdd2db93d4185bb7a4cc7
Alih5690000 <ffffffaaaaaa320@outlook.com>
2026-02-13 17:08:16 +0000

    added Obj idk works

--- main2.c
+++ main2.c
@@ -1,5 +1,28 @@
 #include <Python.h>
 
+typedef struct{
+    PyObject_HEAD
+    int a;
+} Obj;
+
+static PyObject* Obj_new(PyTypeObject* tp,PyObject* args,PyObject* kwargs){
+    Obj* self;
+    self=(Obj*)tp->tp_alloc(tp,0);
+    if (!self) return NULL;
+    int a;
+    if (!PyArg_ParseTuple(args,"i",&a)) return NULL;
+    self->a=a;
+    return self;
+}
+
+static PyTypeObject Obj_t={
+    PyVarObject_HEAD_INIT(NULL,0)
+    .tp_name="Obj",
+    .tp_basicsize=sizeof(Obj),
+    .tp_flags=Py_TPFLAGS_DEFAULT,
+    .tp_new=Obj_new
+};
+
 static PyObject* add(PyObject* self,PyObject* args){
     PyObject* first;
     PyObject* second;
@@ -24,5 +47,6 @@ static struct PyModuleDef moduledef={
 };
 
 PyMODINIT_FUNC PyInit_lib2(void){
+    PyType_Ready(&Obj_t);
     return PyModule_Create(&moduledef);
 }
